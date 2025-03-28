7a052759c19775468eaca7b905b0585768721777
unknown <you@example.com>
2025-03-28 11:44:38 -0500

    DivisionMetodos
    partes de la implementacion

--- Factorial/src/factorial/Factorial.java
+++ Factorial/src/factorial/Factorial.java
@@ -13,9 +13,40 @@ public class Factorial {
     /**
      * @param args the command line arguments
      */
+    //recursivo para calcular el factorial
+    public static long factorialRecursivo(int n) {
+        n = 0;
+    }
+
+    // si un numero es mayor a 0 y no negativos
+    public static boolean esMayorAZero(int n) {
+
+    }
+
+    //verificar si un número es entero (no fracciones ni decimales)
+    public static boolean esEntero(double num) {
+
+    }
+
+    // calcular factorial de números grandes utilizando BigInteger (usa esto)
+    public static java.math.BigInteger factorialGrande(int n) {
+
+    }
+
+    // verificar si es válido (no letras ni caracteres)
+    public static boolean esNumeroValido(String input) {
+
+    }
+
+    // n es la variable global
+
     public static void main(String[] args) {
-        // TODO code application logic here
-        int a =20;
+        // implementacion de los metodos (aqui para la consola)
+        System.out.println("Factorial recursivo: " + factorialRecursivo(numero));
+        System.out.println("Es mayor a 0: " + esMayorAZero(numero));
+        System.out.println("Es entero: " + esEntero(numero));
+        System.out.println("Factorial de número grande: " + factorialGrande(numero));
+        System.out.println("Es número válido: " + esNumeroValido("10"));
     }
     
 }
